6686cc49db5644dce1d936286839200a2a368b43
Micah Mones <micahmones@PC.hitronhub.home>
2019-10-10 19:41:32 -0700

    edit gitignore, add comps and styles folder

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Main from './comps/Main';
+
+const App = () => {
+  return (
+    <View>
+      <Main />
+    </View>
+  )
+}
+
+export default App;
---
+++ comps/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Main Comp</Text>
+    </View>
+  )
+};
+
+export default Main;
