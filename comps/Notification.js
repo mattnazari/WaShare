61d90e0151b63ee9d548479f4cd14ce179059abf
micahmones <micahmones@gmail.com>
2019-10-20 22:32:37 -0700

    building default notification component

--- comps/Notification.js
+++ comps/Notification.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import Header from './Header';
+import styles from '../styles/NotificationStyles';
 
 export const NotificationItem = props => {
   let time = props.date / 60
 
   return (
-    <View>
-      <Text>{time} minutes ago</Text>
-      <Text>{props.message}</Text>
+    <View style={{ alignSelf: 'stretch' }}>
+      <View style={styles.notificationContainer}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.message}>{props.message}</Text>
+        <Text style={styles.timeText}>{time} minutes ago</Text>
+      </View>
     </View>
   )
 }
 
+export function PushNotifications(title, message, time){
+  
+}
+
 const Notification = props => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Header currentTab={'Notifications'} />
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <NotificationItem message={'Hello World!'} date={600}/>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <NotificationItem title={'Another user notified you'} message={'Please pick up your laundry!'} date={600} />
+        <NotificationItem title={'Dryer 2 is out of order'} message={'A repairman will be on his way to fix the dryer. In the meantime, use another one.'} date={600} />
+        <NotificationItem title={'Washer 1 is almost done'} message={'Your washer finishes in 5 minutes. Be ready to pick up your laundry.'} date={600} />
+      </ScrollView>
     </View>
   )
 }
