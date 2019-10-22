7384b04a968193190dd3c464ca6bea13a275e83f
micahmones <micahmones@gmail.com>
2019-10-21 22:33:04 -0700

    building drawer navigator

--- App.js
+++ App.js
@@ -8,11 +8,12 @@ import Main from './comps/Main';
 import Onboarding from './comps/Onboarding';
 import ExtendMachine from './comps/ExtendMachine';
 
-const AppNavigator = createStackNavigator(
-  { //add screens here
+const AppStackNavigator = createStackNavigator(
+  { 
     Main: { screen: Main },
     Onboarding: { screen: Onboarding },
     ExtendMachine: { screen: ExtendMachine },
+    //add screens here that go into normal stack navigation
   },
   {
     headerMode: 'none',
@@ -20,13 +21,20 @@ const AppNavigator = createStackNavigator(
   });
 
 const AppDrawerNavigator = createDrawerNavigator(
-  { //add screens here that go into the hamburger menu
-    Home: { screen: AppNavigator },
+  { 
+    Home: { screen: AppStackNavigator },
     Onboarding: { screen: Onboarding },
     //TO-DO
     //replace these next placeholder screens
     //TO-DO
     'Report Machines': { screen: Onboarding },
+    'Payment Info': { screen: Onboarding },
+    Help: { screen: Onboarding },
+    //add screens here that go into the hamburger menu
+  },
+  {
+    hideStatusBar: true,
+    statusBarAnimation: 'slide'
   })
 
 const AppContainer = createAppContainer(AppDrawerNavigator);
