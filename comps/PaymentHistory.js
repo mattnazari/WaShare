83f1e952dd2fef17482104756fd1a83c72fef300
matthewnazari <matthew.nazari8@gmail.com>
2019-10-24 08:11:27 -0700

    small changes

--- comps/PaymentHistory.js
+++ comps/PaymentHistory.js
@@ -1,16 +1,30 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, TouchableOpacity, Switch} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, Picker} from 'react-native';
 import Header from './Header';
 import styles from '../styles/PaymentHistoryStyles';
 //import {Mastercard} from './SVGComps';
 
 const PaymentHistory = () => {
   const [currentTab, setCurrentTab] = useState('Payment');
+  const [month, setMonth] = useState('October');
+  const [year, setYear] = useState('2019');
 
   return (
     <View style={styles.background}>
       <Header setCurrentTab={setCurrentTab} currentTab={currentTab} />
-      
+        <View style={styles.pickerRow}>
+            <Picker
+            onValueChange={(value)=>{setMonth(value)}}
+            selectedValue={month}
+            >
+            <Picker.Item label='November' value='November'/>
+            <Picker.Item label='December' value='December'/>
+            <Picker.Item label='January' value='January'/>
+            </Picker>
+            <Picker>
+
+            </Picker>
+        </View>
     </View>
   );
 };
---
+++ styles/PaymentHistoryStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'space-between',
+    margin: 0,
+    alignSelf: 'stretch',
+    fontFamily: 'CircularStd-Book',
+  },
+
+  pickerRow:{
+      flexDirection:'row'
+  }
+});
+
+export default styles;
