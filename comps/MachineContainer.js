3f610ee194211f299ba8b59584cceac6a911f0e5
micahmones <micahmones@gmail.com>
2019-11-09 02:31:35 -0800

    working on backend

--- comps/MachineContainer.js
+++ comps/MachineContainer.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import Machine from './Machine';
 import styles from '../styles/MachineStyles';
+import axios from 'axios';
 
 const MachineContainer = props => {
+  const [machines, setMachines] = useState([])
+  const ReadMachines = async () => {
+    var obj = {
+      key: 'machines_read',
+      data: {}
+    }
+
+    let r = await axios.post('http://localhost:3001/post', obj)
+    let dbmachines = JSON.parse(r.data.body)
+    console.log('read', dbmachines)
+    setMachines(dbmachines.data)
+    console.log('reading machines array state', machines)
+  }
+
+  useEffect(() => {
+    ReadMachines()
+  }, [])
+
   return (
     <View>
       <Text style={styles.title}>{props.type} machines</Text>
@@ -18,9 +37,9 @@ const MachineContainer = props => {
           showsVerticalScrollIndicator={false}
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.scrollContainer}>
-          {props.machines.map((machine) =>
-            <Machine key={machine.id}
-              id={machine.id}
+          {machines.map((machine, index) =>
+            <Machine key={index}
+              id={machine.machine_id}
               type={machine.type}
               lock={machine.lock}
               currentTab={props.currentTab}
